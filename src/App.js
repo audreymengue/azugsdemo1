51ce4c83da92e99f54e105a83f708ac6c0c31db4
Audrey Mengue <massoumeharmonie@gmail.com>
2022-05-25 17:02:53 +0000

    azugs demo1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>Hey, CI/CD here with Azure devOps pipeline</h3>
+       
+      </section>
+    </main>
+  )
+}
+
+export default App
