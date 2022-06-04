bc8a8e385101a1209f7200ac7c8fe9e6b91f7fb9
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-04 08:28:28 +0000

    Added new heading again

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
        
         <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
+        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
        
       </section>
     </main>
